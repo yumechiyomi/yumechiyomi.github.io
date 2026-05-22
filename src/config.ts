835086715e53b2f4eebccbbe9b931b91a7a2e158
yumechiyomi <phongtnt43@gmail.com>
2026-05-22 22:04:56 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -49,6 +49,11 @@ export const navBarConfig: NavBarConfig = {
 			url: "https://github.com/yumechiyomi", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
+		{
+			name: "Chat",
+			url: "https://yumechiyomi.github.io/chat-web", // Internal links should not include the base path, as it is automatically added
+			external: true, // Show an external link icon and will open in a new tab
+		},
 	],
 };
 
