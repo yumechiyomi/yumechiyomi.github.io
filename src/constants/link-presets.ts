ad19167a2eb083360b74e22f5bc311b2622749e6
yumechiyomi <phongtnt43@gmail.com>
2026-05-22 22:00:25 +0700

    Update link-presets.ts

--- src/constants/link-presets.ts
+++ src/constants/link-presets.ts
@@ -15,7 +15,7 @@ export const LinkPresets: { [key in LinkPreset]: NavBarLink } = {
 		name: i18n(I18nKey.archive),
 		url: "/archive/",
 	},
-		[LinkPreset.Chat]: {
+	[LinkPreset.Chat]: {
 		name: i18n(I18nKey.archive),
 		url: "/chat/",
 	},
